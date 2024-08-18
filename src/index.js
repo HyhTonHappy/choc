64293606f87a1263abe12669979704a3fc0fa406
HyhTonHappy <tanphuchuynh2547@gmail.com>
2024-08-18 22:03:09 +0700

    choc

---
+++ src/index.js
@@ -0,0 +1,2 @@
+alert("Bạn đã bị theo dõi, chúc mừng bạn đã bị gán mã độc");
+alert("Vào ngày mai bạn sẽ bị mất tất cả như facebook hay tài khoản ngân hàng");
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], plugins: [require('flowbite/plugin')],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+
+  plugins: [
+    require('flowbite/plugin')
+],
+}
